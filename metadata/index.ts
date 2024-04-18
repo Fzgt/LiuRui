3c64b5a54aff7da05e73ccf990186b1f26c52a4a
Fzgt <fzgt320@gmail.com>
2024-04-18 14:22:50 +0930

    feat: change info

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,6 +1,6 @@
 /** 文本 */
-export const siteName = '风中告退'
-export const siteShortName = '风中告退'
+export const siteName = '名古屋'
+export const siteShortName = '名古屋'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
